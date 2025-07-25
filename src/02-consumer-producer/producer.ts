59f3423f8c6d63087d0d879405d31204fbf81f49
Maria Eduarda Sampaio <maria.sampaio@me.com.br>
2025-07-25 20:58:58 -0300

    Add RabbitMQ consumer and producer implementations with necessary dependencies

---
+++ src/02-consumer-producer/producer.ts
@@ -0,0 +1,23 @@
+import * as amqp from "amqplib";
+
+async function producer() {
+    const connection = await amqp.connect('amqp://admin:admin@localhost:5672');
+    const channel = await connection.createChannel();
+
+    const queue = 'hello';
+    const message = 'Hello World!';
+
+    // Ensure the queue exists before sending a message
+    await channel.assertQueue(queue);
+
+    channel.sendToQueue(queue, Buffer.from(message));
+
+    console.log(`[x] Sent ${message}`);
+
+    setTimeout(() => {
+        connection.close();
+        process.exit(0);
+    }, 500);
+}
+
+producer().catch(console.error);
